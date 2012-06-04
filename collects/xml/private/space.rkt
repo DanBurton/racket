07cee5bfdf3ad3152fb6365719902228a872879e
Dan Burton <danburton.email@gmail.com>
2012-06-04 16:00:46 -0600

    typed space.rkt

--- collects/xml/private/space.rkt
+++ collects/xml/private/space.rkt
@@ -1,15 +1,25 @@
-#lang racket
+#lang typed/racket
 (require "structures.rkt")
+#|
 (provide/contract
  [eliminate-whitespace (() ((listof symbol?) (boolean? . -> . boolean?)) . ->* . (element? . -> . element?))])
+|#
+
 
 ;; eliminate-whitespace : (listof Symbol) (Bool -> Bool) -> Element -> Element
-(define (eliminate-whitespace [special empty]
-                              [eliminate-special? (λ (x) x)])
-  (letrec ([blank-it
-            (lambda (el)
-              (let ([name (element-name el)]
-                    [content (map (lambda (x)
+(: eliminate-whitespace
+   (case-lambda [-> (Element -> Element)]
+                [(Listof Symbol) -> Element -> Element]
+                [(Listof Symbol) (Boolean -> Boolean) -> Element -> Element]))
+(define eliminate-whitespace
+  (opt-lambda: ([special : (Listof Symbol) empty]
+                [eliminate-special? : (Boolean -> Boolean)
+                                    (lambda: ([x : Boolean]) x)])
+  (letrec: ([blank-it : (Element -> Element)
+            (lambda: ([el : Element])
+              (let: ([name : Symbol (element-name el)]
+                     [content : (Listof Content)
+                             (map (lambda: ([x : Content])
                                     (if (element? x) (blank-it x) x))
                                   (element-content el))])
                 (make-element
@@ -19,13 +29,17 @@
                  (element-attributes el)
                  (cond
                    [(eliminate-special? (and (memq (element-name el) special) #t))
-                    (filter (lambda (s)
+                    (filter (lambda: ([s : Content])
                               (not (and (pcdata? s)
                                         (or (all-blank (pcdata-string s))
                                             (error 'eliminate-blanks "Element <~a> is not allowed to contain text ~e" name (pcdata-string s))))))
                             content)]
                    [else content]))))])
-    blank-it))
+    blank-it)))
 
 ;; all-blank : String -> Bool
+(: all-blank (String -> Boolean))
 (define (all-blank s) (andmap char-whitespace? (string->list s)))
+
+
+(provide eliminate-whitespace)
